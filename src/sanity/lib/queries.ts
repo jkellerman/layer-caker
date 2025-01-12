fbebc309c952ab0411064d9fde6a414d84191439
jkellerman <89940123+jkellerman@users.noreply.github.com>
2025-01-12 20:54:49 +0000

    create page query

--- src/sanity/lib/queries.ts
+++ src/sanity/lib/queries.ts
@@ -47,3 +47,15 @@ export const POST_QUERY =
     image
   }
 }`);
+
+export const PAGE_QUERY =
+  defineQuery(`*[_type == "page" && slug.current == $slug][0]{
+  ...,
+  content[]{
+    ...,
+    _type == "faqs" => {
+      ...,
+      faqs[]->
+    }
+  }
+}`);
